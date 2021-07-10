2da1fbc10fe3b46e7b54813dc069dd3af1a40a86
Lisa Zhao <84100023+lisazhao30@users.noreply.github.com>
2021-07-10 12:37:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Role Dictionary"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Role Dictionary"
   });
 });
 
